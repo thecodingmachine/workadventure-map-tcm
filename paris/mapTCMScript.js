53d5fc2ed623c955f31dc3aa2aa6159502814396
DESKTOP-FMM8UI0\CLV <c.vieilly@student.isartdigital.com>
2021-04-09 13:59:36 +0200

    Button disable after onClick iframe for the welcome to WA repositionning popups

--- paris/mapTCMScript.js
+++ paris/mapTCMScript.js
@@ -4,14 +4,17 @@ var zoneRespectPeopleName = "popUpRespectPeople";
 console.log("SCRIPT RUNNUNG");
 var urlWelcomeToTCM = "https://www.thecodingmachine.com/en/welcome-to-the-coding-machine/";
 var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
 
 WA.onEnterZone(zoneWelcomeToTCMName, () => {
-   currentPopup =  WA.openPopup("popUpWelcome","Hey ! Welcome to The Coding Machine",[
+   currentPopup =  WA.openPopup("popUpWelcome","Hey ! Welcome to The Coding Machine !",[
         {
             label: "Who are we ? ",
             className: "popUpElement",
             callback: (popup => {
-                WA.openTab(urlWelcomeToTCM);
+                WA.openCoWebSite(urlWelcomeToTCM);
+                isCoWebSiteOpened = true;
+                closePopUp();
             })
         }]);
 })
@@ -21,14 +24,21 @@ WA.onEnterZone(zoneRespectPeopleName, () => {
 })
 
 WA.onEnterZone(zoneMeetTheDevTeamName, () => {
-    currentPopup =  WA.openPopup("popUpMeetDev","Meet the dev team ! ",[]);
+    currentPopup =  WA.openPopup("popUpMeetDev","Meet the WorkAdventure dev team ! ",[]);
 })
 
 WA.onLeaveZone(zoneRespectPeopleName, closePopUp)
 
 WA.onLeaveZone(zoneMeetTheDevTeamName, closePopUp)
 
-WA.onLeaveZone(zoneWelcomeToTCMName, closePopUp)
+WA.onLeaveZone(zoneWelcomeToTCMName, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
 
 function closePopUp(){
     if (currentPopup !== undefined) {

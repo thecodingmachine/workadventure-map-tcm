31cf7110df90ffdd4c9cf7cc4d98948af8abd8a4
moufmouf <d.negrier@thecodingmachine.com>
2021-06-17 16:55:13 +0000

    Deploying to gh-pages from master @ 8e4f673fd5d7dc79fdcacb9d9b8bc0b650c92006 🚀

--- paris/src/floor2.ts
+++ paris/src/floor2.ts
@@ -1,10 +1,9 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 var zoneFootballName = "popupFootballZone";
 
 var currentPopup : any = undefined;
 
 WA.onEnterZone(zoneFootballName, () => {
-    currentPopup = WA.openPopup("popUpFootball", "Congratulations ! You found a ball of the EURO 2021. There is two more to find in our office. If you find them, send the three codes to g.pitaud@workadventu.re. THE CODE : 7EY24", [
+    currentPopup = WA.openPopup("popUpFootball", "Congratulations ! You found a ball of the EURO 2021. There is two more to find in our office. If you find them, send the three codes to g.pitaud@workadventu.re. THE CODE : E466A", [
         {
             label: "Close",
             className: "normal",
@@ -14,10 +13,10 @@ WA.onEnterZone(zoneFootballName, () => {
         }]);
 })
 
-WA.onLeaveZone(zoneFootballName, closePopUp)
+WA.onLeaveZone(zoneFootballName, closePopUp3)
 
 
-function closePopUp(){
+function closePopUp3(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
